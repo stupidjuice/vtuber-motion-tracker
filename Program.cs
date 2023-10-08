6fee3607cd3c90be90bb47c378a279c82bc39ab3
stupidjuice <82068136+stupidjuice@users.noreply.github.com>
2023-10-08 15:24:58 -0700

    i have no clue what i did

--- Program.cs
+++ Program.cs
@@ -2,28 +2,38 @@
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using System.Collections;
-
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 public class Program
 {
     public static void Main(string[] args)
     {
         //----Parameters----
+        int leftCameraIndex = 0; //these are almost CERTAINLY not the same for your system
+        int rightCameraIndex = 1;
         double cutoff = 200;
 
         List<TrackingBox> boxes = new List<TrackingBox>();
-        TrackingBox test = new TrackingBox(80, 80, 40, 255, 255, 0);
+        TrackingBox test = new TrackingBox(120, 120, 30, 255, 255, 0);
         boxes.Add(test);
 
         CvInvoke.NamedWindow("test");
         using (Mat frame = new Mat())
-        using (VideoCapture capture = new VideoCapture())
+        using (Mat frame2 = new Mat())
+        using (VideoCapture capture = new VideoCapture(leftCameraIndex))
+        using (VideoCapture capture2 = new VideoCapture(rightCameraIndex))
             while (CvInvoke.WaitKey(1) == -1)
             {
                 capture.Read(frame);
+                capture2.Read(frame2);
                 Image<Bgr, Byte> img = frame.ToImage<Bgr, Byte>();
+                Image<Bgr, Byte> img2 = frame2.ToImage<Bgr, Byte>();
 
-                foreach (TrackingBox box in boxes)
+                Console.WriteLine(boxes[0].x);
+                Parallel.For(0, boxes.Count, i =>
                 {
+                    TrackingBox box = boxes[i];
                     //-----DRAW BOXES-----//
                     for (int x = box.x - box.radius; x < box.x + box.radius; x++)
                     {
@@ -48,14 +58,14 @@ public class Program
                     List<int> yList = new List<int>(); //lists are used to find medians.
                     List<int> xList = new List<int>(); //there's probably a better way but idk
                     int medianX = 0, medianY = 0;
-                    int numX = 0, numY = 0;
+
                     for (int x = box.x - box.radius + 1; x < box.x + box.radius; x++)
                     {
                         for (int y = box.y - box.radius + 1; y < box.y + box.radius; y++)
                         {
                             double pixelLuminance = 0.2126 * img.Data[x, y, 2] + 0.7152 * img.Data[x, y, 1] + 0.0722 * img.Data[x, y, 0];
 
-                            if(pixelLuminance > cutoff)
+                            if (pixelLuminance > cutoff)
                             {
                                 xList.Add(x);
                                 yList.Add(y);
@@ -80,7 +90,8 @@ public class Program
                     box.x = medianX; box.y = medianY;
 
                     img[medianX, medianY] = new Bgr(0.0, 0.0, 255.0);
-                }
+                });
+
                 CvInvoke.Imshow("test", img);
             }
     }
